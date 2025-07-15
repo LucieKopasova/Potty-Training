ecabeefca29b22ffc91813377ab40e741333ecd1
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-07-15 20:56:47 +0200

    UseState for alarmDelay

--- src/components/AlarmDelayMenu.jsx
+++ src/components/AlarmDelayMenu.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
 export const AlarmDelayMenu = ({ closeMenu }) => {
+  const [alarmDelay, setAlarmDelay] = useState(0);
+
+  const handlePostpone = (number) => {
+    setAlarmDelay(Number(number));
+  };
+  console.log(alarmDelay);
+
   return (
     <div className="alarmDelayMenu-container">
       <div className="alarmDelayMenu-header">Odložit o:</div>
-      <button
-        className="alarmDelayMenu-btn" /* onClick={() => handlePostpone(5)} */
-      >
+      <button className="alarmDelayMenu-btn" onClick={() => handlePostpone(5)}>
         +5 min
       </button>
-      <button
-        className="alarmDelayMenu-btn" /* onClick={() => handlePostpone(10)} */
-      >
+      <button className="alarmDelayMenu-btn" onClick={() => handlePostpone(10)}>
         +10 min
       </button>
-      <button
-        className="alarmDelayMenu-btn" /* onClick={() => handlePostpone(15)} */
-      >
+      <button className="alarmDelayMenu-btn" onClick={() => handlePostpone(15)}>
         +15 min
       </button>
       <button className="alarmDelayMenu-btn close-menu" onClick={closeMenu}>
